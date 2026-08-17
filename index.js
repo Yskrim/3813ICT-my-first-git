49af89f4a6f3aab46badb581aec753885619fc9c
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-08-17 22:54:17 +1000

    task5: kept both changes -- completed

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ console.log("Hello world!");
 const addFunc = (a, b) => a + b;
 console.log(addFunc(5, 10));
 
-<<<<<<< HEAD
 console.log("Part 2 of the task");
-=======
-console.log("I edited this file lol"):
->>>>>>> ef492783dc8f27701b94ceaa1251b61c4afffc28
+console.log("I edited this file lol");
